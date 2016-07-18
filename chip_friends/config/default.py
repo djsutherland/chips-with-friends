93a9d00010ebed061bb45600ecb2c524cc55acbc
Dougal J. Sutherland <dougal@gmail.com>
2016-07-17 22:31:00 -0400

    add a reminder to override SECRET_KEY :)

--- chip_friends/config/default.py
+++ chip_friends/config/default.py
@@ -1,8 +1,10 @@
 import os
 
+ADMINS = ['dougal@gmail.com']
 DATABASE = 'sqlite:///' + os.path.abspath(
     os.path.join(os.path.dirname(__file__), '../../chip_friends.db'))
+
+# override this in deploy.py!
 SECRET_KEY = '9Zbl48DxpawebuOKcTIxsIo7rZhgw2U5qs2mcE5Hqxaa7GautgOh3rkvTabKp'
-ADMINS = ['dougal@gmail.com']
 
 DEBUG = True
